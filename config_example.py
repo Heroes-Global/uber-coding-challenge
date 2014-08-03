4098bd1435813188ca22ac88d1216d90e68cb9a1
Peter Urbak <peter@dragonwasrobot.com>
2014-08-03 14:53:39 +0200

    Added heroku setup files.

--- config_example.py
+++ config_example.py
@@ -22,8 +22,12 @@ SF_MOVIES_DATBASE = "<INSERT_DATABASE_NAME>"
 
 # # SQLAlchemy
 
-SQLALCHEMY_DATABASE_URI = "postgresql://" + SF_MOVIES_USER + \
-                          ":" + SF_MOVIES_PASSWORD + \
-                          "@" + SF_MOVIES_HOST + \
-                          "/" + SF_MOVIES_DATBASE
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = "postgresql://" + SF_MOVIES_USER + \
+                              ":" + SF_MOVIES_PASSWORD + \
+                              "@" + SF_MOVIES_HOST + \
+                              "/" + SF_MOVIES_DATBASE
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
---
+++ runp-heroku.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# Imports
+
+from app import app
